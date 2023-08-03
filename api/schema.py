6a647c87cf289767b005667bd0856f92731eeb99
jimmykamau <jimmykambiz@gmail.com>
2023-08-03 19:15:50 +0300

    Update required fields

--- api/schema.py
+++ api/schema.py
@@ -3,10 +3,10 @@ from marshmallow import Schema, fields
 
 class PredictRequestSchema(Schema):
     current_account_balance = fields.Float(required=True)
-    general_governemnt_structural_balance = fields.Float(required=True)
+    average_consumer_price_inflation_index = fields.Float(required=True)
     employment = fields.Float(required=True)
     implied_ppp_conversion_rate = fields.Float(required=True)
-    consumer_price_index = fields.Float(required=True)
+    unemployment_rate = fields.Float(required=True)
     continent = fields.String(required=True)
     population = fields.Float(required=True)
     gross_nation_savings = fields.Float(required=True)
